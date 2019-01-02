26ea9aa782c06d71770db2d4e33775afda10948d
Michael O'Halloran <C-Michael.OHalloran@charter.com>
2019-01-01 23:12:25 -0700

    adding dynamic server routing

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute
+    ) { }
+
+  ngOnInit() {
+    // console.log('queryParams: ', this.route.snapshot.queryParams);
+    // console.log('fragment: ', this.route.snapshot.fragment);
+    // console.log('snapshot: ', this.route.snapshot);
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
